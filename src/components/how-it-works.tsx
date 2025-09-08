ce7772d41f6f838b89219cbd456f079fce43e873
wlaj <wlajansen16@gmail.com>
2025-09-08 18:28:09 +0200

    Update corners to be rounded

--- src/components/how-it-works.tsx
+++ src/components/how-it-works.tsx
@@ -1,4 +1,11 @@
-import { IconCircle1, IconCircle2, IconCircle3, IconNumber1, IconNumber2, IconNumber3 } from "@tabler/icons-react";
+import {
+  IconCircle1,
+  IconCircle2,
+  IconCircle3,
+  IconNumber1,
+  IconNumber2,
+  IconNumber3,
+} from "@tabler/icons-react";
 import React from "react";
 
 type HowItWorks = {
@@ -23,21 +30,23 @@ const HowItWorks = ({ heading, demoUrl, demoTitle }: HowItWorks) => {
           </a>
         </div>
         <div className="flex gap-4 items-center">
-          <div className="flex flex-col gap-2 p-6 border w-1/3">
+          <div className="flex flex-col gap-2 rounded-xl p-6 border w-1/3">
             <IconCircle1 className="size-8" />
-            <h3 className="text-xl font-medium">Zoek op locatie of voeg je locatie toe</h3>
+            <h3 className="text-xl font-medium">
+              Zoek op locatie of voeg je locatie toe
+            </h3>
             <p className="text-neutral-600">
               Vul je postcode in om restaurants in jouw buurt te vinden.
             </p>
           </div>
-          <div className="flex flex-col gap-2 p-6 border w-1/3">
+          <div className="flex flex-col gap-2 rounded-xl p-6 border w-1/3">
             <IconCircle2 className="size-8" />
             <h3 className="text-xl font-medium">Ontdek lokale restaurants</h3>
             <p className="text-neutral-600">
               Bekijk een overzicht van alle restaurants bij jou in de buurt.
             </p>
           </div>
-          <div className="flex flex-col gap-2 p-6 border w-1/3">
+          <div className="flex flex-col gap-2 p-6 rounded-xl border w-1/3">
             <IconCircle3 className="size-8" />
             <h3 className="text-xl font-medium">Bestel en geniet</h3>
             <p className="text-neutral-600">
