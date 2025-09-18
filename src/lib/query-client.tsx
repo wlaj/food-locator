99912e65fdbaaaedaee7ea472c033bc1fa047498
wlaj <wlajansen16@gmail.com>
2025-09-18 20:34:43 +0200

    Remove turbopack from pnpm run dev

--- src/lib/query-client.tsx
+++ src/lib/query-client.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 
 const queryClient = new QueryClient({
@@ -19,13 +18,6 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {process.env.NODE_ENV === 'development' && (
-        <ReactQueryDevtools 
-          initialIsOpen={false} 
-          buttonPosition="bottom-left"
-          position="left"
-        />
-      )}
     </QueryClientProvider>
   )
 }
